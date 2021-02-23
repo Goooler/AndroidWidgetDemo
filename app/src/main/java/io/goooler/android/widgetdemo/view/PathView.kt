ff6697d7b9b6145a6a8916351a589801443683be
Goooler <wangzongler@gmail.com>
2021-02-23 17:59:23 +0800

    Add PathView

---
+++ app/src/main/java/io/goooler/android/widgetdemo/view/PathView.kt
@@ -0,0 +1,29 @@
+package io.goooler.android.widgetdemo.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.View
+
+class PathView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
+
+  private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+  private val path = Path()
+
+  init {
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = 4f
+  }
+
+  override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+    path.lineTo(200f, 200f)
+    path.lineTo(200f, 0f)
+  }
+
+  override fun onDraw(canvas: Canvas) {
+    canvas.translate(width / 2f, height / 2f)
+    canvas.drawPath(path, paint)
+  }
+}
